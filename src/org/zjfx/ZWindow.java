74f18259ef07f419febb3451ff157c4432690868
Anatoliy Sablin <sablintolya@gmail.com>
2012-07-31 23:11:14 +0400

    fix path to default css

--- src/org/zjfx/ZWindow.java
+++ src/org/zjfx/ZWindow.java
@@ -66,7 +66,7 @@ public abstract class ZWindow<T> {
                 .center(createForm())
                 .build();
         final Scene scene = SceneBuilder.create().root(borderPane).fill(Color.TRANSPARENT).build();
-        scene.getStylesheets().add(getClass().getResource("zwindow.css").toExternalForm());
+        scene.getStylesheets().add(ClassLoader.getSystemResource("org/zjfx/zwindow.css").toExternalForm());
         return scene;
     }
 
